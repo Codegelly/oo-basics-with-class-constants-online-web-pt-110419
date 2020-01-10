fb7c86e4b057a4eb28b6632601dfcb5dc5be6bab
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-10 01:44:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
+<<<<<<< HEAD
   attr_reader :brand
     BRANDS = []
+=======
+  attr_reader :brand, :unique_brands 
+    UNIQUE_BRANDS = []
+>>>>>>> 221cb77dbd1d7769e8acd1c4fa70b73c746819fa
     
   def initialize(brand)
     @brand = brand
@@ -14,9 +19,15 @@ end
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+<<<<<<< HEAD
   def brand= (brand)
     @brand = brand
     BRANDS << brand
+=======
+  def unique_brands= (unique_brands)
+    @unique_brands = unique_brands 
+    UNIQUE_BRANDS << unique_brands
+>>>>>>> 221cb77dbd1d7769e8acd1c4fa70b73c746819fa
   end 
   
 end
